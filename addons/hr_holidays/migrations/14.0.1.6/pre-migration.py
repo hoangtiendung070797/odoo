ce2ef363a31a9d89ba88af207439b1490e8cd055
hoangtiendung <hoangtiendung070797@gmail.com>
2022-03-25 14:03:58 +0700

    [FIX]hr_holidays: refactor code

--- addons/hr_holidays/migrations/14.0.1.6/pre-migration.py
+++ addons/hr_holidays/migrations/14.0.1.6/pre-migration.py
@@ -2,6 +2,6 @@ def migrate(cr, version):
     cr.execute("""
         UPDATE hr_leave
         SET request_unit_custom = FALSE
-        WHERE request_unit_custom = TRUE 
-        AND  holiday_status_id IS NOT NULL OR request_unit_half OR request_unit_hours;
+        WHERE request_unit_custom = TRUE
+        AND (holiday_status_id IS NOT NULL OR request_unit_half OR request_unit_hours);
     """)
